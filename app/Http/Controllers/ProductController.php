1a6b0785aeff3bbab45d0d3b03209308a7036e4c
belal-allala <belalallala810@gmail.com>
2025-03-10 10:45:15 +0000

    Ajouter les méthodes CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,40 +9,25 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        return Product::all();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    
+    public function store(Request $request) {
+        return Product::create($request->all());
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+    
+    public function show(Product $product) {
+        return $product;
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+    
+    public function update(Request $request, Product $product) {
+        $product->update($request->all());
+        return $product;
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    
+    public function destroy(Product $product) {
+        $product->delete();
+        return response()->noContent();
     }
 }
